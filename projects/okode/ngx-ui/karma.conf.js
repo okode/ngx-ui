d0f66a38e5774be0914fb0ae4489bae4c5ce6887
Pedro Jorquera <pjorquera@okode.com>
2022-05-07 12:54:40 +0200

    Fixing tests in GH Action

--- projects/okode/ngx-ui/karma.conf.js
+++ projects/okode/ngx-ui/karma.conf.js
@@ -53,7 +53,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
